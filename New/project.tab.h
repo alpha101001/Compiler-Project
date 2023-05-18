2826fa456a4e62a731e040e4d564a3eb4beaaf41
Avishek Roy <102016083+alpha101001@users.noreply.github.com>
2023-05-18 13:36:37 +0600

    Add files via upload

---
+++ New/project.tab.h
@@ -0,0 +1,94 @@
+
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     NUM = 258,
+     var = 259,
+     IF = 260,
+     ELSE = 261,
+     BS = 262,
+     BE = 263,
+     floop = 264,
+     wloop = 265,
+     SWITCH = 266,
+     CASE = 267,
+     DEFAULT = 268,
+     major = 269,
+     print = 270,
+     INT = 271,
+     CHAR = 272,
+     FLOAT = 273,
+     array = 274,
+     odd_even = 275,
+     factorial = 276,
+     class = 277,
+     function = 278,
+     SIN = 279,
+     COS = 280,
+     LOG = 281,
+     TAN = 282
+   };
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+
+/* Line 1676 of yacc.c  */
+#line 13 "project.y"
+ 
+  int v;
+  double d;   
+
+
+
+/* Line 1676 of yacc.c  */
+#line 86 "project.tab.h"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+
